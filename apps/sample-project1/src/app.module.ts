1fe8b57900fe2112f90b7555c1c09768d0fc9e53
Yasuyuki Saito <yasuyuki.saito.2012@gmail.com>
2020-04-30 09:20:25 +0900

    lib追加

--- apps/sample-project1/src/app.module.ts
+++ apps/sample-project1/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+
+import { SampleLibModule } from '@monorepo-lib/sample-lib';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [SampleLibModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- libs/sample-lib/src/sample-lib.service.ts
+++ libs/sample-lib/src/sample-lib.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class SampleLibService {}
+export class SampleLibService {
+  getMessage(): string {
+    return 'Sample Lib message';
+  }
+}
